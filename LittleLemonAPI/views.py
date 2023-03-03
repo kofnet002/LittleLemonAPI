7569327c8d309b56cb83c3917b45c94de5184cb4
frimpong-kofi <noeljoel61@gmail.com>
2023-03-03 18:32:02 +0000

    Commented the codes

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -10,7 +10,12 @@ from django.test import Client
 
 
 # Create your views here.
+
+#------------------------------------------------------------
+# Manaing Food Menu Items
+#------------------------------------------------------------
 class MenuItemView(APIView):
+    # Get method for user's with view permission
     def get(self, request):
         menu_items = MenuItem.objects.all()
         serializer = MenuItemSerializer(menu_items, many=True)
@@ -18,6 +23,7 @@ class MenuItemView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
+    # Post method for Managers to add food item
     def post(self, request):
         if request.user.groups.filter(name="Manager"):
             data = MenuItemSerializer(data=request.data)
@@ -29,17 +35,21 @@ class MenuItemView(APIView):
         return Response("Not authorized...", status=status.HTTP_403_FORBIDDEN)
 
 
+# Menu Item detail
 class MenuItemDetail(APIView):
+    # Getting a particular menu item
     def get_object(self, id):
         try:
             return MenuItem.objects.get(id=id)
         except MenuItem.DoesNotExist:
             pass
     
+    # Get the detail of a particular food menu
     def get(self, request, id):
         menu_item = MenuItemSerializer(self.get_object(id))
         return Response(menu_item.data, status=status.HTTP_200_OK)
 
+    # Update method for Managers to update a particular food item
     def put(self, request, id):
         menu_item = self.get_object(id)
         data = MenuItemSerializer(menu_item, data=request.data)
@@ -49,6 +59,7 @@ class MenuItemDetail(APIView):
                 return Response(data.data, status=status.HTTP_201_CREATED)
         return Response("Not authorised to make changes", status=status.HTTP_403_FORBIDDEN)
 
+    # Delete method for Managers to remove a particular food item
     def delete(self, request, id):
         menu_item = self.get_object(id)
         if request.user.groups.filter(name="Manager"):
@@ -57,7 +68,11 @@ class MenuItemDetail(APIView):
         return Response("Not authorised to remove item", status=status.HTTP_403_FORBIDDEN)
 
 
+# --------------------------------------------------------------
+# Managing User Groups
+#---------------------------------------------------------------
 class UserGroupManagement(APIView):
+    # Getting all user groups
     def get(self, request):
         users = User.objects.filter(groups__name="Manager")
         users_data = UserSerializer(users, many=True)
@@ -65,7 +80,7 @@ class UserGroupManagement(APIView):
             return Response(users_data.data)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
-#    Assigns the user in the payload to the manager group and returns 201-Created
+    # Assigns the user in the payload to the manager group and returns 201-Created
     def post(self, request, format=None):
         if request.user.groups.filter(name="Manager"):
             serializer = UserSerializer(data=request.data)
@@ -80,11 +95,13 @@ class UserGroupManagement(APIView):
 
 class RemoveUserFromManagerGroup(APIView):
     def delete(self, request, id):
+        # Getting a particular user object
         try:
             user = User.objects.get(id=id)
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
+        # Removing a user from the manager group by a manager
         group = Group.objects.get(name='Manager')
         if request.user.groups.filter(name="Manager"):
             if group in user.groups.all():
@@ -96,6 +113,7 @@ class RemoveUserFromManagerGroup(APIView):
 
 
 class DeliveryCrewManagerGroup(APIView):
+    # Getting all delivery crew group members
     def get(self, request):
         users = User.objects.filter(groups__name="Delivery crew")
         users_data = UserSerializer(users, many=True)
@@ -103,6 +121,7 @@ class DeliveryCrewManagerGroup(APIView):
             return Response(users_data.data)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
     
+    # Adding a user to delivery crew group through the payload
     def post(self, request, format=None):
         if request.user.groups.filter(name="Manager"):
             serializer = UserSerializer(data=request.data)
@@ -122,6 +141,7 @@ class RemoveUserFromDeliveryCrewGroup(APIView):
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
+        # Remove user from the deliveryCrewGroup
         group = Group.objects.get(name='Delivery crew')
         if request.user.groups.filter(name="Manager"):
             if group in user.groups.all():
@@ -132,7 +152,11 @@ class RemoveUserFromDeliveryCrewGroup(APIView):
         return Response("Not authorized", status=status.HTTP_401_UNAUTHORIZED)
 
 
+#------------------------------------------------
+# Managing the cart view
+#------------------------------------------------
 class CartView(APIView):
+    # Getting all the cart items that belong to the signed user
     def get(self, request):
         user = request.user
         carts = Cart.objects.filter(user=user)  
@@ -141,6 +165,7 @@ class CartView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
 
+    # Adding item to cart by a customer
     def post(self, request):
         if request.user.groups.filter(name="customer"):
             serializer = CartSerializer(data=request.data)
@@ -152,12 +177,14 @@ class CartView(APIView):
 
 
 class RemoveCartItem(APIView):
+    # Getting a particular cart item by the signed in user/customer
     def get_object(self, id):
         try:
             return Cart.objects.get(id=id)
         except Cart.DoesNotExist:
             return Response("Cart object not found", status=status.HTTP_404_NOT_FOUND)
     
+    # removing a cart item by the signed in user/customer
     def delete(self, request, id):
         if request.user.groups.filter(name="customer"):
             cart = self.get_object(id)
@@ -168,20 +195,27 @@ class RemoveCartItem(APIView):
         return Response("Not authorized to remove cart!", status=status.HTTP_401_UNAUTHORIZED)
 
 
+#-------------------------------------------------
+# Managing order items
+#-------------------------------------------------
 class OrderView(APIView):
+    # Getting the order items by the authenticated user
     def get(self, request):
         user = request.user
         orders = Order.objects.filter(user=user)
         serializer = OrderSerializer(orders, many=True)
 
+    # order items by the authenticated user
         if request.user.groups.filter(name="customer"):
             return Response(serializer.data, status=status.HTTP_200_OK)
-            
+
+    # all order items       
         elif request.user.groups.filter(name="Manager"):
             all_orders = OrderItem.objects.all()
             all_orders_serializer = OrderItemSerializer(all_orders, many=True)
             return Response(all_orders_serializer.data, status=status.HTTP_200_OK)
-        
+
+    # all order items assigned to a particular delivery crew
         elif request.user.groups.filter(name="Delivery crew"):
             # Get all orders with order items assigned to the delivery crew
             orders = Order.objects.filter(delivery_crew=request.user)
@@ -216,6 +250,7 @@ class OrderView(APIView):
 
 
 class OrderDetail(APIView):
+    # Getting a particular order item
     def get_object(self, id):
         try:
             return Order.objects.get(id=id)
@@ -223,6 +258,7 @@ class OrderDetail(APIView):
             pass
             # return Response('Order object not found', status=status.HTTP_404_NOT_FOUND)
 
+    # Particular order item detail by a customer
     def get(self, request, id):
         order_item = OrderSerializer(self.get_object(id))
         # user = order_item.data.get('user').get('username')
@@ -232,6 +268,7 @@ class OrderDetail(APIView):
         else:
             return Response("Not authorized to view this page", status=status.HTTP_401_UNAUTHORIZED)
     
+    # Update order item by a customer or manager
     def put(self, request, id):
         order_item = OrderSerializer(self.get_object(id))
         if request.user.groups.filter(name="customer" or "Manager"):
@@ -241,6 +278,7 @@ class OrderDetail(APIView):
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        # Update the status of the particular order item when delivered
         elif request.user.groups.filter(name="Delivery crew"):
             order = OrderSerializer(order_item, data=request.data)
             if order.is_valid():
@@ -248,7 +286,8 @@ class OrderDetail(APIView):
                 return Response("Updated successfully", status=status.HTTP_201_CREATED)
             return Response(order.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response("Not authorized", status=status.HTTP_401_UNAUTHORIZED)
-        
+    
+    # Remove a particular order item by the manager
     def delete(self, request, id):
         if request.user.groups.filter(name="Manager"):
             order_item = self.get_object(id)
